f9e02269db28d0ad1d471b19b7e5a52d1cc30e1c
Jason Divinagracia <jmarkd04@gmail.com>
2023-11-08 12:12:57 -0500

    Optimization - geometries and materials

--- src/components/Level/BlockStart.jsx
+++ src/components/Level/BlockStart.jsx
@@ -1,8 +1,29 @@
+import * as THREE from "three";
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+const floor1Material = new THREE.MeshStandardMaterial({ color: "limegreen" });
+const floor2Material = new THREE.MeshStandardMaterial({ color: "greenyellow" });
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: "orangered" });
+const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
+
+/**
+ *
+ * @param {position}
+ * @returns Floor mesh
+ */
+
 export default function BlockStart({ position = [0, 0, 0] }) {
   return (
     <group position={position}>
-      <mesh position={[0, -0.1, 0]} receiveShadow>
-        <boxGeometry args={[4, 0.2, 4]} />
+      {/* Floor */}
+      <mesh
+        geometry={boxGeometry}
+        scale={[4, 0.2, 4]}
+        position={[0, -0.1, 0]}
+        material={floor1Material}
+        receiveShadow
+      >
         <meshStandardMaterial color="limegreen" />
       </mesh>
     </group>
