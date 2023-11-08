c1d4818d9a08cb09767dfb532794025033835f5a
Jason Divinagracia <jmarkd04@gmail.com>
2023-11-08 11:01:15 -0500

    Setup - vite.js, normal scene with lights, install dependencies

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(
+  <React.StrictMode>
+    <Canvas
+      shadows
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [2.5, 4, 6],
+      }}
+    >
+      <Experience />
+    </Canvas>
+  </React.StrictMode>
+);
