8717bb0e73aaba5a235393deb30635e8c51e4af3
Jason Divinagracia <jmarkd04@gmail.com>
2023-11-10 16:34:48 -0500

    Implement logic to shuffle the blocks

--- src/components/Level/index.jsx
+++ src/components/Level/index.jsx
@@ -3,15 +3,28 @@ import BlockStart from "./BlockStart";
 import BlockLimbo from "./BlockLimbo";
 import BlockWall from "./BlockWall";
 import BlockEnd from "./BlockEnd";
+import { useMemo } from "react";
 
-export default function Level() {
+export default function Level({
+  count = 5,
+  types = [BlockWall, BlockLimbo, BlockSpinner],
+}) {
+  const blocks = useMemo(() => {
+    const blocks = [];
+    for (let i = 0; i < count; i++) {
+      const type = types[Math.floor(Math.random() * types.length)];
+      blocks.push(type);
+    }
+    return blocks;
+  }, [count, types]);
+
   return (
     <>
-      <BlockStart position={[0, 0, 16]} />
-      <BlockSpinner position={[0, 0, 12]} />
-      <BlockLimbo position={[0, 0, 8]} />
-      <BlockWall position={[0, 0, 4]} />
-      <BlockEnd position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 0]} />
+      {blocks.map((Block, index) => (
+        <Block key={index} position={[0, 0, -(index + 1) * 4]} />
+      ))}
+      <BlockEnd position={[0, 0, -(count + 1) * 4]} />
     </>
   );
 }
