fc445199d449eb17361014853391cd6d804c1fcb
Jason Divinagracia <jmarkd04@gmail.com>
2023-11-13 16:23:30 -0500

    Setup Controls - using KeyboardControls helper from Drei

--- src/index.jsx
+++ src/index.jsx
@@ -4,22 +4,33 @@ import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
 import { Perf } from "r3f-perf";
+import { KeyboardControls } from "@react-three/drei";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-  <React.StrictMode>
-    <Canvas
-      shadows
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [2.5, 4, 6],
-      }}
-    >
-      <Experience />
-      <Perf openByDefault trackGPU={true} position={"bottom-left"} />
-    </Canvas>
-  </React.StrictMode>
+  <KeyboardControls
+    map={[
+      { name: "forward", keys: ["ArrowUp", "KeyW"] },
+      { name: "backward", keys: ["ArrowDown", "KeyS"] },
+      { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+      { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+      { name: "jump", keys: ["Space"] },
+    ]}
+  >
+    <React.StrictMode>
+      <Canvas
+        shadows
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [2.5, 4, 6],
+        }}
+      >
+        <Experience />
+        <Perf openByDefault trackGPU={true} position={"bottom-left"} />
+      </Canvas>
+    </React.StrictMode>
+  </KeyboardControls>
 );
