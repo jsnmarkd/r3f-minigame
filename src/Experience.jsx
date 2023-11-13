cafd5dd900f4d5a7532b0fde3c6a1da823d3e15c
Jason Divinagracia <jmarkd04@gmail.com>
2023-11-13 18:19:46 -0500

    Setup Controls - implement player movement

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -8,7 +8,7 @@ export default function Experience() {
     <>
       <OrbitControls makeDefault />
 
-      <Physics debug>
+      <Physics debug={false}>
         <Lights />
         <Level />
       </Physics>
