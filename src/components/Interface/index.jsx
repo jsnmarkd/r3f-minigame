7e0006eae70d2e9064a3e652ec7215c385bd134f
Jason Divinagracia <jmarkd04@gmail.com>
2023-11-16 13:27:13 -0500

    Setup Game Mechanics - Implement the Reset button and only show when the player has reached the end

--- src/components/Interface/index.jsx
+++ src/components/Interface/index.jsx
@@ -1,6 +1,10 @@
 import { useKeyboardControls } from "@react-three/drei";
+import useGame from "../../stores/useGame";
 
 export default function Interface() {
+  const restart = useGame((state) => state.restart);
+  const phase = useGame((state) => state.phase);
+
   const forward = useKeyboardControls((state) => state.forward);
   const backward = useKeyboardControls((state) => state.backward);
   const leftward = useKeyboardControls((state) => state.leftward);
@@ -13,7 +17,11 @@ export default function Interface() {
       <div className="time">0.00</div>
 
       {/* Restart */}
-      <div className="restart">Restart</div>
+      {phase === "ended" && (
+        <div className="restart" onClick={restart}>
+          Restart
+        </div>
+      )}
 
       {/* Controls */}
       <div className="controls">
